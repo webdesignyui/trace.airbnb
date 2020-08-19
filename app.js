fe784932b82e3eebe93daf60f809a656ce17a6c4
國部佑衣 <yui8888@kunibuyuinoMacBook-Air.local>
2020-08-19 19:10:43 +0900

    スクロール時のヘッダー・元サイトの画像とテキスト変更に対応しました

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
-// $(function () {
-//   $('.js-toggleButton').click(function () {
-//     $('.box1').toggleClass('text');
-//   });
-//   $('.js-toggleButton').click(function () {
-//     $('.box1').css('opacity', '1');
-//   });
-
-// });
-
 $(function () {
+
+
+  // box1
+  // --------------------------------------
+
   $('.js-toggleButton').click(function () {
     $('.box1').fadeToggle(100);
   });
+
+  $('.js-toggleButton2').click(function () {
+    $('.box2').fadeToggle(100);
+  });
+
+
+  // header
+  // --------------------------------------
+
+  $('#header').each(function () {
+    var $window = $(window),
+      $stickyHeader = $(this).find('.sticky-header'),//スティッキーヘッダー
+      stickyHeaderHeight = $stickyHeader.outerHeight(),//スティッキーヘッダーの高さ
+      headerHeight = $(this).outerHeight();//ヘッダー全体の高さ
+
+    //画面外へ
+    $stickyHeader.css({ top: '-' + stickyHeaderHeight + 'px' });
+
+    //ページの一番上からヘッダーの高さ分下方向にスクロールしたらtopを0に、それ以外は画面外へ
+    $window.on('scroll', function () {
+      if ($window.scrollTop() > headerHeight) {
+        $stickyHeader.css({ top: 0 });
+      } else {
+        $stickyHeader.css({ top: '-' + stickyHeaderHeight + 'px' });
+      }
+    });
+
+    //任意のタイミングでイベントを発生させる
+    $window.trigger('scroll');
+  });
+
+
 
 });
