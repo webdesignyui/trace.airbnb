5c6b344f4e744493375b26f5d507eff9a9c71516
國部佑衣 <yui8888@kunibuyuinoMacBook-Air.local>
2020-08-19 13:10:52 +0900

    ヘッダーの言語選択にjQueryで動きをつけました

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+// $(function () {
+//   $('.js-toggleButton').click(function () {
+//     $('.box1').toggleClass('text');
+//   });
+//   $('.js-toggleButton').click(function () {
+//     $('.box1').css('opacity', '1');
+//   });
+
+// });
+
 $(function () {
-
-
-
-
+  $('.js-toggleButton').click(function () {
+    $('.box1').fadeToggle(100);
+  });
 
 });
